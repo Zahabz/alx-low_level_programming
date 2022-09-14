34bca73f66c86ac4e033d6ce42fe6a37eebf68a0
Zahabz <herbert.bett54@gmail.com>
2022-09-14 17:21:04 +0300

    Last digit computation

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@ int print_last_digit(int n)
 {
 	int z;
 
-	if (n > 0 && n > 10)
+	if (n > 0 && n >= 10)
 	{
 		z = n % 10;
 
 	}
-	else if (n < 0 && n < -10)
+	else if (n < 0 && n <= -10)
 	{
 		z = (-1 * n) % 10;
 
